0ad8a1ea918859847a3969e4de4ad4534f08c6a5
Messias Dias <messiasdias.ti@gmail.com>
2020-11-16 19:04:47 -0300

    Modified github action workflows

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
-wget https://getcomposer.org/composer-stable.phar &&
-sudo mv composer-stable.phar /bin/composer &&
+#wget https://getcomposer.org/composer-stable.phar &&
+#sudo mv composer-stable.phar /bin/composer &&
 echo ENV_TEST=ok > .env &&
 composer test
