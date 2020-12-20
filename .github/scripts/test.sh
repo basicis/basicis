5899d8f85aec0b4f6416b8cb14614633b279934c
Messias Dias <messiasdias.ti@gmail.com>
2020-12-20 18:00:09 -0300

    Update Example classes according basics/core

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-touch bin/basicis.db &&
+touch storage/basicis.db &&
 composer doctrine orm:schema-tool:create &&
 echo APP_ENV=dev > .env &&
 echo DB_DRIVER=pdo_sqlite > .env &&
-echo DB_PATH=bin/basicis.db > .env &&
+echo DB_PATH=storage/basicis.db > .env &&
 composer test
-rm bin/basicis.db
+rm storage/basicis.db
