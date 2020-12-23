86cfda21ead05aacafcf39ebee6169722118b6c7
Messias Dias <messiasdias.ti@gmail.com>
2020-12-22 21:11:40 -0300

    Update github action test script

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 touch storage/basicis.db &&
 composer doctrine orm:schema-tool:create &&
-echo APP_ENV=dev > .env.test &&
-echo DB_DRIVER=pdo_sqlite > .env.test &&
-echo DB_PATH=storage/basicis.db > .env.test &&
+echo APP_ENV=dev >> .env &&
+echo DB_DRIVER=pdo_sqlite >> .env &&
+echo DB_PATH=storage/basicis.db >> .env &&
 composer phpcs &&
 composer test &&
+rm .env
 rm storage/basicis.db
