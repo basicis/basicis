f8a1575b78044b104b8aa6f8ef57b46ab27e3f19
Messias Dias <messiasdias.ti@gmail.com>
2020-12-22 22:34:24 -0300

    Update github action test script, .gitignore, composer.json, remove .env.test and adding script project-install

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+touch .env.test &&
+echo APP_ENV=dev > .env.test &&
+echo DB_DRIVER=pdo_sqlite >> .env.test &&
+echo DB_PATH=storage/basicis.db >> .env.test &&
+
 touch storage/basicis.db &&
-composer doctrine orm:schema-tool:create &&
-echo APP_ENV=dev >> .env &&
-echo DB_DRIVER=pdo_sqlite >> .env &&
-echo DB_PATH=storage/basicis.db >> .env &&
-composer phpcs &&
-composer test &&
-rm .env
+vendor/bin/doctrine orm:schema-tool:create &&
+vendor/bin/phpcs --standard=PSR2 src/ test/ &&
+vendor/bin/phpunit --do-not-cache-result --no-logging test &&
+
+rm .env.test &&
 rm storage/basicis.db
